c0831df7c039e3b6cfcef1489467ef621214603c
SuggyJr <127042729+SuggyJr@users.noreply.github.com>
2023-03-21 16:51:19 +0000

    Add files via upload

---
+++ A01 - Alfie Sugden/CMP1124M - Algorithms and Complexity - A01/CMP1124M - Aclgorithms and Complexity - A01/searchAndSort.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace CMP1124M___Aclgorithms_and_Complexity___A01
+{
+    public static class searchAndSort
+    {
+        public static void searchTrafficData()
+        {
+            //
+        }
+
+        public static void sortTrafficDataAsc(int[] array) 
+        {
+            Array.Sort(array);
+            Console.WriteLine("Sorted Ascending");
+        }
+
+        public static void sortTrafficDataDesc(int[] array)
+        {
+            Array.Sort(array);
+            Array.Reverse(array);
+            Console.WriteLine("Sorted Descending");
+        }
+    }
+}
